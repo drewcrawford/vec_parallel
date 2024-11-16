51b14c9de4870c2ea21083f5e5608711e6466b02
Drew Crawford <drew@sealedabstract.com>
2024-11-15 18:40:59 -0600

    Sketch implementation

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,41 @@ enum Strategy {
     One
 }
 
-struct VecTask {
-
+struct SliceTask<'a,T,B> {
+    slice: &'a mut [T],
+    build: B,
 }
+
+trait BuildFn<T> {
+
+    fn build<'a>(&'a mut self, index: usize) -> impl Future<Output=T> + 'a;
+}
+
+struct RunFnFuture<'a, F> {
+    f: &'a mut F,
+    index: usize,
+}
+
+impl <F,R> Future for RunFnFuture<'_,F> where F: FnMut(usize) -> R {
+    type Output = R;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let index = self.index;
+        Poll::Ready((self.f)(index))
+    }
+}
+
+impl<'a, R,F> BuildFn<R> for F where F: FnMut(usize) -> R + 'a, Self: 'a {
+
+    fn build(&mut self, index: usize) -> RunFnFuture<F> {
+        RunFnFuture {
+            f: self,
+            index,
+        }
+    }
+}
+
+
 
 struct VecResult<I> {
     phantom_data: PhantomData<I>
@@ -25,15 +57,18 @@ impl<I> Future for VecResult<I> {
     }
 }
 
-struct VecBuilder<I> {
-    tasks: Vec<VecTask>,
+struct VecBuilder<I: 'static,B> {
+    tasks: Vec<SliceTask<'static, I,B>>,
     result: VecResult<I>,
 }
-pub fn build_vec<R>(len: usize, strategy: Strategy, f: impl FnMut(usize) -> R) -> VecBuilder<R> {
-
+pub fn build_vec<R,B>(len: usize, strategy: Strategy, f: B) -> VecBuilder<R,B> {
+    let mut vec = Vec::with_capacity(len);
     match strategy {
         Strategy::One => {
-            let task = VecTask {};
+            let task = SliceTask {
+                slice: &mut vec,
+                build: f,
+            };
             let result = VecResult {phantom_data: PhantomData};
             VecBuilder {
                 tasks: vec![task],
@@ -45,8 +80,13 @@ pub fn build_vec<R>(len: usize, strategy: Strategy, f: impl FnMut(usize) -> R) -
 }
 
 #[cfg(test)] mod tests {
+    use crate::VecResult;
+
     #[test] fn test_build_vec() {
-        let builder = super::build_vec(10, super::Strategy::One, |i| i);
-        test_executors::spin_on(builder.result);
+        let builder = super::build_vec(10, super::Strategy::One, |i: usize| i);
+        for task in builder.tasks {
+            todo!()
+        }
+        test_executors::spin_on::<VecResult<i32>>(builder.result);
     }
 }
